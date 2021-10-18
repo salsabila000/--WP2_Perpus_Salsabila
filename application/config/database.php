d962814e973767d40f653036df6fa7ab7e769c85
salsabila000 <salsabilaramadaniah1@gmail.com>
2021-10-18 16:48:00 +0700

    upload tugas pertemuan 6 membuat database

--- application/config/database.php
+++ application/config/database.php
@@ -72,13 +72,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
-
+ 
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => '',
+	'username' => 'root',
 	'password' => '',
-	'database' => '',
+	'database' => 'perpustakaan',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
